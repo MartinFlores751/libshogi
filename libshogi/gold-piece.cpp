5637015bbe0624a4277c37b003907cdf96834c4d
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-07-30 18:01:01 -0500

    Implement GoldPiece movement

--- libshogi/gold-piece.cpp
+++ libshogi/gold-piece.cpp
@@ -4,6 +4,18 @@ namespace shogi {
 GoldPiece::GoldPiece(int x, int y, PieceColor color)
     : ShogiPiece(x, y, color) {}
 
-bool GoldPiece::move(int newX, int newY) noexcept { return false; }
+bool GoldPiece::move(int newX, int newY) noexcept {
+  std::pair<int, int> pos{getPosition()};
+  int forwardDistance{newY - pos.second};
+  // Handle foreward cases
+  if (forwardDistance == 1 || forwardDistance == 0) {
+    if (newX >= pos.first - 1 || newX <= pos.first + 1) {
+      return true;
+    }
+  } else if (forwardDistance == -1 && newX == pos.first) {
+    return true;
+  }
+  return false;
+}
 void GoldPiece::promote() noexcept {}
 } // namespace shogi
