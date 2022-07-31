73e821389c709ead3599d10ff59d006a76ed7b20
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-07-30 20:00:50 -0500

    Implement Siver General movement

--- libshogi/silver-piece.cpp
+++ libshogi/silver-piece.cpp
@@ -7,12 +7,15 @@ SilverPiece::SilverPiece(int x, int y, PieceColor color)
 bool SilverPiece::move(int newX, int newY) noexcept {
   std::pair<int, int> pos{getPosition()};
   int forwardDistance{newY - pos.second};
+  int sideDistance{newX - pos.first};
+
   // Handle foreward cases
   if (forwardDistance == 1) {
     if (newX >= pos.first - 1 || newX <= pos.first + 1) {
       return true;
     }
-  } else if (forwardDistance == -1 && newX == pos.first) {
+  } else if (forwardDistance == -1 &&
+             (sideDistance == 1 || sideDistance == -1)) {
     return true;
   }
   return false;
