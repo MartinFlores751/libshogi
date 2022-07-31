7cc065683b41e7cb403a4f24247d9bf8b04475f6
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-07-31 18:15:31 -0500

    Add Pawn to build
    Increment CMake version
    
    Implement Pawn movement

--- libshogi/pawn-piece.cpp
+++ libshogi/pawn-piece.cpp
@@ -8,6 +8,9 @@ PawnPiece::PawnPiece(int x, int y, PieceColor color)
 bool PawnPiece::move(int newX, int newY) noexcept {
   std::pair<int, int> pos{getPosition()};
 
+  if (newY == pos.first + 1)
+    return true;
+
   return false;
 }
 void PawnPiece::promote() noexcept {}
