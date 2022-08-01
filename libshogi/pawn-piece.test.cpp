ccfc442a636bfcf18044805c3a45df529b0b1057
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-07-31 19:03:16 -0500

    Add sided pawn tests

--- libshogi/pawn-piece.test.cpp
+++ libshogi/pawn-piece.test.cpp
@@ -15,7 +15,11 @@ template <> struct StringMaker<std::pair<int, int>> {
 };
 } // namespace Catch
 
-SCENARIO("pawn moving in a normal pattern") {
+// -----------------
+// Black Piece tests
+// -----------------
+
+SCENARIO("black pawn moving in a normal pattern") {
   GIVEN("A central pawn piece") {
     std::unique_ptr<shogi::ShogiPiece> centerPiece =
         std::make_unique<shogi::PawnPiece>(
@@ -30,7 +34,7 @@ SCENARIO("pawn moving in a normal pattern") {
   }
 }
 
-SCENARIO("pawn moving in non-standad pattern") {
+SCENARIO("black pawn moving in non-standad pattern") {
   GIVEN("A central pawn piece") {
     std::unique_ptr<shogi::ShogiPiece> centerPiece =
         std::make_unique<shogi::PawnPiece>(
@@ -52,6 +56,54 @@ SCENARIO("pawn moving in non-standad pattern") {
 
     WHEN("the pawn is moved backward") {
       centerPiece->movePiece(5, 4);
+      THEN("the pawn is not moved") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 5});
+      }
+    }
+  }
+}
+
+// -----------------
+// White piece tests
+// -----------------
+
+SCENARIO("white pawn moving in a normal pattern") {
+  GIVEN("A central pawn piece") {
+    std::unique_ptr<shogi::ShogiPiece> centerPiece =
+        std::make_unique<shogi::PawnPiece>(
+            5, 5, shogi::ShogiPiece::PieceColor::White);
+
+    WHEN("the pawn is moved forward") {
+      centerPiece->movePiece(5, 4);
+      THEN("the pawn moves forward") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 4});
+      }
+    }
+  }
+}
+
+SCENARIO("white pawn moving in non-standad pattern") {
+  GIVEN("A central pawn piece") {
+    std::unique_ptr<shogi::ShogiPiece> centerPiece =
+        std::make_unique<shogi::PawnPiece>(
+            5, 5, shogi::ShogiPiece::PieceColor::White);
+
+    WHEN("the pawn is moved far forward") {
+      centerPiece->movePiece(5, 3);
+      THEN("the pawn is not moved") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 5});
+      }
+
+      WHEN("the pawn is moved left") {
+        centerPiece->movePiece(6, 5);
+        THEN("the pawn is not moved") {
+          REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 5});
+        }
+      }
+    }
+
+    WHEN("the pawn is moved backward") {
+      centerPiece->movePiece(5, 6);
       THEN("the pawn is not moved") {
         REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 5});
       }
