429689cff04494d1e720c410a31c44fc73598761
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-08-02 17:34:32 -0500

    Update bishop movement

--- libshogi/bishop-piece.cpp
+++ libshogi/bishop-piece.cpp
@@ -1,19 +1,18 @@
 #include <libshogi/bishop-piece.hpp>
-#include <math.h>
 
 namespace shogi {
 BishopPiece::BishopPiece(int x, int y, PieceColor color)
     : ShogiPiece(x, y, color) {}
 
 bool BishopPiece::move(int newX, int newY) noexcept {
-  std::pair<int, int> pos{getPosition()};
-
-  if ((newX == pos.first) || (newY == pos.second)) {
+  if (newX == 0 || newY == 0) {
     return false;
   }
 
-  float slope = std::abs((newX - pos.first) / (newY - pos.second));
-  return slope == 1 ? true : false;
+  // Remainder greater than 0 implies not a 1/1 slope
+  int remainder = newX % newY;
+
+  return remainder == 0 ? true : false;
 }
 void BishopPiece::promote() noexcept {}
 } // namespace shogi
