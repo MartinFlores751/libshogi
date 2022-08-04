7007d83c5314a54e4003c472a40576d7eb57c66c
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-08-04 17:56:19 -0500

    Add white piece lance tests

--- libshogi/lance-piece.test.cpp
+++ libshogi/lance-piece.test.cpp
@@ -15,7 +15,11 @@ template <> struct StringMaker<std::pair<int, int>> {
 };
 } // namespace Catch
 
-SCENARIO("lance moving in a normal pattern") {
+// ----------------
+// Black piece test
+// ----------------
+
+SCENARIO("black lance moving in a normal pattern") {
   GIVEN("A central lance piece") {
     std::unique_ptr<shogi::ShogiPiece> centerPiece =
         std::make_unique<shogi::LancePiece>(
@@ -30,7 +34,7 @@ SCENARIO("lance moving in a normal pattern") {
   }
 }
 
-SCENARIO("lance moving in non-standad pattern") {
+SCENARIO("black lance moving in non-standad pattern") {
   GIVEN("A central lance piece") {
     std::unique_ptr<shogi::ShogiPiece> centerPiece =
         std::make_unique<shogi::LancePiece>(
@@ -53,6 +57,53 @@ SCENARIO("lance moving in non-standad pattern") {
       cornerPiece->movePiece(1, 10);
       THEN("the lance remains in place") {
         REQUIRE(cornerPiece->getPosition() == std::pair<int, int>{1, 1});
+      }
+    }
+  }
+}
+
+// ----------------
+// White piece test
+// ----------------
+
+SCENARIO("white lance moving in a normal pattern") {
+  GIVEN("A central lance piece") {
+    std::unique_ptr<shogi::ShogiPiece> centerPiece =
+        std::make_unique<shogi::LancePiece>(
+            5, 5, shogi::ShogiPiece::PieceColor::White);
+
+    WHEN("the lance is moved up") {
+      centerPiece->movePiece(5, 3);
+      THEN("the lance moves up") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 3});
+      }
+    }
+  }
+}
+
+SCENARIO("white lance moving in non-standad pattern") {
+  GIVEN("A central lance piece") {
+    std::unique_ptr<shogi::ShogiPiece> centerPiece =
+        std::make_unique<shogi::LancePiece>(
+            5, 5, shogi::ShogiPiece::PieceColor::White);
+
+    WHEN("the lance is moved backward") {
+      centerPiece->movePiece(5, 6);
+      THEN("the lance is not moved") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 5});
+      }
+    }
+  }
+
+  GIVEN("A corner lance piece") {
+    std::unique_ptr<shogi::ShogiPiece> cornerPiece =
+        std::make_unique<shogi::LancePiece>(
+            9, 9, shogi::ShogiPiece::PieceColor::White);
+
+    WHEN("the lance is moved off the board") {
+      cornerPiece->movePiece(9, 0);
+      THEN("the lance remains in place") {
+        REQUIRE(cornerPiece->getPosition() == std::pair<int, int>{9, 9});
       }
     }
   }
