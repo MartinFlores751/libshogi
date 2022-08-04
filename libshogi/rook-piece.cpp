fb2658a85f79b13eb597ca39b1118ba6f73949b1
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-08-04 17:44:34 -0500

    Add comments clarifying logic

--- libshogi/rook-piece.cpp
+++ libshogi/rook-piece.cpp
@@ -5,7 +5,10 @@ RookPiece::RookPiece(int x, int y, PieceColor color)
     : ShogiPiece(x, y, color) {}
 
 bool RookPiece::move(int newX, int newY) noexcept {
-  return (newX == 0 && newY != 0) || (newY == 0 && newX != 0) ? true : false;
+  return (newX == 0 && newY != 0)            // Range forward or back
+                 || (newY == 0 && newX != 0) // Range left or right
+             ? true
+             : false;
 }
 void RookPiece::promote() noexcept {}
 } // namespace shogi
