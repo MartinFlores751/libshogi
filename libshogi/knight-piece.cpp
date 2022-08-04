29ebd41ebbc1fada5d8ef0afbaedbb8e5ee75a54
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-08-04 18:12:21 -0500

    Update knight movement

--- libshogi/knight-piece.cpp
+++ libshogi/knight-piece.cpp
@@ -5,13 +5,9 @@ KnightPiece::KnightPiece(int x, int y, PieceColor color)
     : ShogiPiece(x, y, color) {}
 
 bool KnightPiece::move(int newX, int newY) noexcept {
-  std::pair<int, int> pos{getPosition()};
-
-  if ((newY == (pos.second + 2)) &&
-      (newX == pos.first - 1 || newX == pos.first + 1)) {
+  if ((newY == 2) && (newX == -1 || newX == 1)) {
     return true;
   }
-
   return false;
 }
 void KnightPiece::promote() noexcept {}
