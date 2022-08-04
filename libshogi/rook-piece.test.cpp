2abec03796d3b0af543e2f907a44cb5d8aa72b53
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-08-04 17:33:36 -0500

    Add white piece rook test

--- libshogi/rook-piece.test.cpp
+++ libshogi/rook-piece.test.cpp
@@ -15,7 +15,11 @@ template <> struct StringMaker<std::pair<int, int>> {
 };
 } // namespace Catch
 
-SCENARIO("rook moving in a normal pattern") {
+// -----------------
+// Black piece tests
+// -----------------
+
+SCENARIO("black rook moving in a normal pattern") {
   GIVEN("A central rook piece") {
     std::unique_ptr<shogi::ShogiPiece> centerPiece =
         std::make_unique<shogi::RookPiece>(
@@ -37,7 +41,7 @@ SCENARIO("rook moving in a normal pattern") {
   }
 }
 
-SCENARIO("rook moving in non-standad pattern") {
+SCENARIO("black rook moving in non-standad pattern") {
   GIVEN("A central rook piece") {
     std::unique_ptr<shogi::ShogiPiece> centerPiece =
         std::make_unique<shogi::RookPiece>(
@@ -55,6 +59,60 @@ SCENARIO("rook moving in non-standad pattern") {
     std::unique_ptr<shogi::ShogiPiece> cornerPiece =
         std::make_unique<shogi::RookPiece>(
             1, 1, shogi::ShogiPiece::PieceColor::Black);
+
+    WHEN("the rook is moved off the board") {
+      cornerPiece->movePiece(0, 1);
+      THEN("the rook remains in place") {
+        REQUIRE(cornerPiece->getPosition() == std::pair<int, int>{1, 1});
+      }
+    }
+  }
+}
+
+// -----------------
+// White piece tests
+// -----------------
+
+SCENARIO("white rook moving in a normal pattern") {
+  GIVEN("A central rook piece") {
+    std::unique_ptr<shogi::ShogiPiece> centerPiece =
+        std::make_unique<shogi::RookPiece>(
+            5, 5, shogi::ShogiPiece::PieceColor::White);
+
+    WHEN("the rook is moved forward") {
+      centerPiece->movePiece(5, 2);
+      THEN("the rook moves forward") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 2});
+      }
+    }
+
+    WHEN("the rook is moved backward") {
+      centerPiece->movePiece(5, 9);
+      THEN("the rook moves backward") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 9});
+      }
+    }
+  }
+}
+
+SCENARIO("white rook moving in non-standad pattern") {
+  GIVEN("A central rook piece") {
+    std::unique_ptr<shogi::ShogiPiece> centerPiece =
+        std::make_unique<shogi::RookPiece>(
+            5, 5, shogi::ShogiPiece::PieceColor::White);
+
+    WHEN("the rook is moved up-left") {
+      centerPiece->movePiece(4, 4);
+      THEN("the rook remains in place") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 5});
+      }
+    }
+  }
+
+  GIVEN("A corner rook piece") {
+    std::unique_ptr<shogi::ShogiPiece> cornerPiece =
+        std::make_unique<shogi::RookPiece>(
+            1, 1, shogi::ShogiPiece::PieceColor::White);
 
     WHEN("the rook is moved off the board") {
       cornerPiece->movePiece(0, 1);
