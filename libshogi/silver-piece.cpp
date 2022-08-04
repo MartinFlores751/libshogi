596c81df4d205f16adf1067236677ea22be61e33
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-08-04 16:45:51 -0500

    Update silver general movement

--- libshogi/silver-piece.cpp
+++ libshogi/silver-piece.cpp
@@ -5,17 +5,9 @@ SilverPiece::SilverPiece(int x, int y, PieceColor color)
     : ShogiPiece(x, y, color) {}
 
 bool SilverPiece::move(int newX, int newY) noexcept {
-  std::pair<int, int> pos{getPosition()};
-  int forwardDistance{newY - pos.second};
-  int sideDistance{newX - pos.first};
-
-  // Handle foreward cases
-  if (forwardDistance == 1) {
-    if (newX >= pos.first - 1 || newX <= pos.first + 1) {
-      return true;
-    }
-  } else if (forwardDistance == -1 &&
-             (sideDistance == 1 || sideDistance == -1)) {
+  if (newY == 1 && (newX >= -1 || newX <= +1)) {
+    return true;
+  } else if (newY == -1 && (newX == 1 || newX == -1)) {
     return true;
   }
   return false;
