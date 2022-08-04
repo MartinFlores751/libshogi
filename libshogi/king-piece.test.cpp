971fcb14f9b2e8a9bd0cb348715a273e5dae83fd
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2022-08-04 17:46:39 -0500

    Add white king piece tests

--- libshogi/king-piece.test.cpp
+++ libshogi/king-piece.test.cpp
@@ -15,7 +15,11 @@ template <> struct StringMaker<std::pair<int, int>> {
 };
 } // namespace Catch
 
-SCENARIO("king moving in a normal pattern") {
+// --------------------
+// Black piece movement
+// --------------------
+
+SCENARIO("black king moving in a normal pattern") {
   GIVEN("A central king piece") {
     std::unique_ptr<shogi::ShogiPiece> centerPiece =
         std::make_unique<shogi::KingPiece>(
@@ -44,7 +48,7 @@ SCENARIO("king moving in a normal pattern") {
   }
 }
 
-SCENARIO("king moving in non-standad pattern") {
+SCENARIO("black king moving in non-standad pattern") {
   GIVEN("A central king piece") {
     std::unique_ptr<shogi::ShogiPiece> centerPiece =
         std::make_unique<shogi::KingPiece>(
@@ -67,6 +71,67 @@ SCENARIO("king moving in non-standad pattern") {
       cornerPiece->movePiece(0, 0);
       THEN("the king remains in place") {
         REQUIRE(cornerPiece->getPosition() == std::pair<int, int>{1, 1});
+      }
+    }
+  }
+}
+
+// --------------------
+// White piece movement
+// --------------------
+
+SCENARIO("white king moving in a normal pattern") {
+  GIVEN("A central king piece") {
+    std::unique_ptr<shogi::ShogiPiece> centerPiece =
+        std::make_unique<shogi::KingPiece>(
+            5, 5, shogi::ShogiPiece::PieceColor::White);
+
+    WHEN("the king is moved forward") {
+      centerPiece->movePiece(5, 4);
+      THEN("the king moves forward") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 4});
+      }
+    }
+
+    WHEN("the king is moved backward") {
+      centerPiece->movePiece(5, 6);
+      THEN("the king moves backward") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 6});
+      }
+    }
+
+    WHEN("the king is moved up-left") {
+      centerPiece->movePiece(4, 4);
+      THEN("the king moves up-left") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{4, 4});
+      }
+    }
+  }
+}
+
+SCENARIO("white king moving in non-standad pattern") {
+  GIVEN("A central king piece") {
+    std::unique_ptr<shogi::ShogiPiece> centerPiece =
+        std::make_unique<shogi::KingPiece>(
+            5, 5, shogi::ShogiPiece::PieceColor::White);
+
+    WHEN("the king is moved too far forward") {
+      centerPiece->movePiece(5, 3);
+      THEN("the king is not moved") {
+        REQUIRE(centerPiece->getPosition() == std::pair<int, int>{5, 5});
+      }
+    }
+  }
+
+  GIVEN("A corner king piece") {
+    std::unique_ptr<shogi::ShogiPiece> cornerPiece =
+        std::make_unique<shogi::KingPiece>(
+            9, 9, shogi::ShogiPiece::PieceColor::Black);
+
+    WHEN("the king is moved off the board") {
+      cornerPiece->movePiece(10, 10);
+      THEN("the king remains in place") {
+        REQUIRE(cornerPiece->getPosition() == std::pair<int, int>{9, 9});
       }
     }
   }
